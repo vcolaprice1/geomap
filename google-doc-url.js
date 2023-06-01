5151b2234b78d592353c56b52e70f95a8b4315ca
vcolaprice1 <107610805+vcolaprice1@users.noreply.github.com>
2023-06-01 10:26:42 +0200

    Initial commit

---
+++ google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Nxrmw8U4TQ1zLByEGLqsjsMrgTGn3BsN-SwvoNtA17M/edit#gid=0';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
