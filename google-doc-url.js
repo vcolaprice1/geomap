7f82f5bb5b580d18df2e09956b593dbe50a6281d
vcolaprice1 <107610805+vcolaprice1@users.noreply.github.com>
2023-06-01 10:38:15 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1fnNhR2o8W7xoLml2SRwXlsODUkckE6oVwQmnVexjThI/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1fnNhR2o8W7xoLml2SRwXlsODUkckE6oVwQmnVexjThI/edit#gid=0'; 
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
